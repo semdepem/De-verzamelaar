906ad62a1ed9d8f0099dcd107653c505130a483c
semdepem <88699@glr.nl>
2023-09-29 12:02:13 +0200

    cool

--- PHP/Producten.php
+++ PHP/Producten.php
@@ -4,8 +4,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="/CSS/index.css">
-    <title>Home</title>
+    <link rel="stylesheet" href="/CSS/producten.css">
+    <title>Producten</title>
   </head>
   <body>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
@@ -17,10 +17,10 @@
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link" aria-current="page" href="/PHP/index.php">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/PHP/Producten.php">Producten</a>
+          <a class="nav-link active"  aria-current="page"  href="#">Producten</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="/PHP/reviews.php">Reviews</a>
@@ -37,9 +37,7 @@
     <h1>Lorem Ipsun</h1>
 </div>
 
-<footer>
-  
-</footer>
+
 <!-- bootstrap scripts -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
