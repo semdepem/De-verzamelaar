9c9a16883bb9cc98149b77cf282f9d323045ec80
semdepem <88699@glr.nl>
2023-10-06 10:54:58 +0200

    UFKC PHP LOGIN

--- PHP/index.php
+++ PHP/index.php
@@ -47,7 +47,7 @@ $conn = new mysqli($servername, $username, $password);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
+//echo "Connected successfully";
 ?>
 
 <div class="scrollable-container">
